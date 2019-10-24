46bacd9a1acee64ef5435e4234e259066fda7864
Leon Tea <leon.tea@hotmail.com>
2019-10-24 16:34:42 +0200

    created FlightEndpoint

---
+++ TravelAgencyEe/src/main/java/at/htl/travelagency/InitBean.java
@@ -0,0 +1,4 @@
+package at.htl.travelagency;
+
+public class InitBean {
+}
---
+++ TravelAgencyEe/src/main/java/at/htl/travelagency/boundary/FlightEndpoint.java
@@ -0,0 +1,8 @@
+package at.htl.travelagency.boundary;
+
+import javax.ws.rs.Path;
+
+@Path("/")
+public class FlightEndpoint {
+
+}
