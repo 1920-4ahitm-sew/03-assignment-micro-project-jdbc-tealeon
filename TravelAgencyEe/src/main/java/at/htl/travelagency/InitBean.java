5cb3d7a81e78973c15e1d03a02bb3bbf4b4f0ab2
Leon Tea <leon.tea@hotmail.com>
2019-10-24 16:36:27 +0200

    created InitBean

--- TravelAgencyEe/src/main/java/at/htl/travelagency/InitBean.java
+++ TravelAgencyEe/src/main/java/at/htl/travelagency/InitBean.java
@@ -1,4 +1,24 @@
 package at.htl.travelagency;
 
+import at.htl.travelagency.entity.Flight;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.Initialized;
+import javax.enterprise.event.Observes;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.util.Date;
+
+@ApplicationScoped
+@Transactional
 public class InitBean {
+    @PersistenceContext
+    EntityManager em;
+
+    public void init(@Observes @Initialized(ApplicationScoped.class) Object init){
+        System.out.println("IT WORKS!");
+        Flight outboundFlight = new Flight("LNZ", "LAX", "25.10.2019");
+        Flight returnFlight = new Flight("LAX", "LNZ", "29.10.2019");
+    }
 }
